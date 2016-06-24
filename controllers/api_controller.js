b549cc31461533ab20b581e4bdc1a44babd96402
khalidrahmani <khalid.rahmani.mail@gmail.com>
2016-06-24 15:36:07 +0000

    fc

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -2,84 +2,97 @@ var express = require('express')
 	 ,router  = express.Router()
 	 ,CREDENTIALS  = require('../config/config')
 	 ,async        = require('async')
+	 ,moment       = require('moment')
 	 ,pg           = require('pg')
 	 ,Sequelize    = require('sequelize')
-	 ,sequelize    = new Sequelize(CREDENTIALS.db_url);
-	 //,sequelize = new Sequelize('postgres://'+CREDENTIALS.pg.user+':'+CREDENTIALS.pg.password+'@'+CREDENTIALS.pg.host+':'+CREDENTIALS.pg.port+'/'+CREDENTIALS.pg.database);
+	 ,sequelize    = new Sequelize(CREDENTIALS.db_url)
+	 ,itemTypes    = {facebook: 2, youtube: 3, instagram: 4, twitter: 5, pinterest: 6, tumblr: 7}
+	 ,Item = sequelize.define('item', {
+				itemtypeid: 			Sequelize.INTEGER,
+				areaid:           Sequelize.INTEGER,
+				sourceid: 		  	Sequelize.STRING,
+				title:    				Sequelize.TEXT,
+				description: 			Sequelize.TEXT,
+				sourceurl: 				Sequelize.STRING,
+				sourcecreatedutc: Sequelize.DATE,
+				viewcount:  			Sequelize.INTEGER,
+				coordinatexy:  		Sequelize.STRING,
+				labelalignment:  	Sequelize.STRING,				
+			  itemid: {
+			    type: Sequelize.INTEGER,
+			    primaryKey: true,
+			    autoIncrement: true
+		  	}		
+			}, {
+				timestamps: true, 
+				updatedAt: false,
+				createdAt: 'createdutc',		
+				freezeTableName: true,
+			})
 
-
-
+/*
 function pushToArray(_array, itemType, sourceID, title, description, sourceCreatedUTC, sourceUrl) {
     var temp = {};
     temp.itemType = itemType;
     temp.sourceID = sourceID;
     temp.title = title;
     temp.description = description;
-    temp.sourceCreatedUTC = sourceCreatedUTC;
+    temp.sourcecreatedutc = sourceCreatedUTC;
     temp.sourceUrl = sourceUrl;
     _array.push(temp);
     return _array;
 }
+*/
 
-/*
-for the item type
-itemType ==2 //facebook
-itemType ==4 //instagram
-itemType ==6 //pinterest
-itemType ==5 //twitter
-itemType ==3 //youtube
-itemType ==7 //tumblr
- */
-
-router.get('/', function(req, res, next) {
-	var Item = sequelize.define('item', {
-		sourceid: Sequelize.STRING,
-		title:    Sequelize.TEXT,
-		description: Sequelize.TEXT,
-		itemtypeid: Sequelize.INTEGER,
-		viewcount:  Sequelize.INTEGER,
-		coordinatexy:  Sequelize.STRING,
-		labelalignment:  Sequelize.STRING,
-		sourcecreatedutc:  Sequelize.DATE,
-	  itemid: {
-	    type: Sequelize.INTEGER,
-	    primaryKey: true,
-	    autoIncrement: true
-  	}		
-	}, {
-		timestamps: true, 
-		updatedAt: false,
-		createdAt: 'createdutc',		
-		freezeTableName: true,
-	});
-	sequelize.sync().then(function() {
-	  return Item.create({
-    	sourceid: 2,
-		title:    'title',
-		description: "<hello> w/ook ",
-		itemtypeid: 2,
-		viewcount:  0,
-		coordinatexy:  '52622450',
-		labelalignment:  'labelalignment',
-		sourcecreatedutc:  new Date(),
-	  });
-	}).then(function(item) {
-	  console.log("done !");
-	  res.send({status: "200", data: item.get({plain: true})})
-	});	
-})
+function pushToArray(_array, itemtypeid, sourceid, title, description, sourcecreatedutc, sourceurl) {
+  var temp = {}
+  temp.itemtypeid 			= itemTypes[itemtypeid]
+  temp.sourceid 				= sourceid.toString()
+  temp.title 						= title || ''
+  temp.description 			= description  || ''
+  temp.sourcecreatedutc = sourcecreatedutc
+  temp.sourceurl 				= sourceurl
+  //temp.areaid           = 24 // need to have data in area table and areatype
+	temp.viewcount 				= 0
+	temp.coordinatexy 		= '(70,4)'
+	temp.labelalignment 	= 'LEFT'
+  _array.push(temp)
+  return _array
+}
 
 router.get('/2', function(req, res, next) {
+	Item.bulkCreate([  { itemtypeid: 3,
+    sourceid: 'icrz-7SdP0s',
+    title: 'Lariss - Coca cola the voice happy energy tour 2016',
+    description: '',
+    sourcecreatedutc: "2016-06-24T12:08:49.435Z",
+    sourceurl: 'https://i.ytimg.com/vi/icrz-7SdP0s/default.jpg',
+    viewcount: 0,
+    coordinatexy: '',
+    labelalignment: '' },
+  { itemtypeid: 2,
+    sourceid: '40796308305_958069170957792',
+    title: '',
+    description: '',
+    sourcecreatedutc: "2016-06-24T12:08:49.435Z",
+    sourceurl: '',
+    viewcount: 0,
+    coordinatexy: '',
+    labelalignment: '' } ]).then(function(item) {
+	  		console.log("done !");
+	  		res.send({status: "200", data: item})
+		});	
+})
+
+router.get('/', function(req, res, next) {
 	async.waterfall([
     function(callback) {
   		data = []
       var ig = require('instagram-node').instagram(); //3263523015,249655166
       ig.use({ access_token: CREDENTIALS.instagram.access_token });
       ig.user_media_recent(CREDENTIALS.instagram.brandId, function(err, result, remaining, limit) {  
-        if (result) {                 
-          var temp = Number('' + result[0].created_time + '000');
-          var time = (new Date(temp)).toUTCString();                 
-          data = pushToArray(data, 4, result[0].id, result[0].caption, result[0].caption, time, result[0].link)
+        if (result.length > 0) {              
+          data = pushToArray(data, 'instagram', result[0].id, result[0].caption, result[0].caption, moment(result[0].created_time, 'x'), result[0].link)
         }
         callback(null, data);
       });
@@ -92,7 +105,7 @@ router.get('/2', function(req, res, next) {
           var temp = new Date();
           var date = temp.toUTCString();
           var array = result.data;
-          data = pushToArray(data, 6, array[0].id, array[0].note, array[0].note, date, array[0].link)
+          data = pushToArray(data, 'pinterest', array[0].id, array[0].note, array[0].note, date, array[0].link)
         }
         callback(null, data);
       });		    	
@@ -108,7 +121,7 @@ router.get('/2', function(req, res, next) {
 
       twitter_client.get('search/tweets', { q: CREDENTIALS.twitter.title }, function(error, tweets, response) {                    
         if (tweets.statuses[0]) {
-        	data = pushToArray(data, 5, tweets.statuses[0].id, tweets.statuses[0].text, tweets.statuses[0].text, tweets.statuses[0].created_at, '')
+        	data = pushToArray(data, 'twitter', tweets.statuses[0].id, tweets.statuses[0].text, tweets.statuses[0].text, tweets.statuses[0].created_at, '')
         }
         callback(null, data);
       });		    	
@@ -137,7 +150,7 @@ router.get('/2', function(req, res, next) {
           description = snippet.description;
           sourceCreatedUTC = snippet.publishedAt;
           sourceUrl = snippet.thumbnails.default.url;
-          data = pushToArray(data, 3, sourceID, title, description, sourceCreatedUTC, sourceUrl)
+          data = pushToArray(data, 'youtube', sourceID, title, description, sourceCreatedUTC, sourceUrl)
         }
         callback(null, data);
       });		    	
@@ -154,7 +167,7 @@ router.get('/2', function(req, res, next) {
       var blog = new tumblr.Blog(CREDENTIALS.tumblr.brandTitle, oauth);
       blog.text({ limit: 1 }, function(error, response) {
           if (response.posts[0]) {
-          	data = pushToArray(data, 7, response.posts[0].id, response.posts[0].title, response.posts[0].blog_name, response.posts[0].date, response.posts[0].post_url)
+          	data = pushToArray(data, 'tumblr', response.posts[0].id, response.posts[0].title, response.posts[0].blog_name, response.posts[0].date, response.posts[0].post_url)
           }
           callback(null, data);
       });		    	
@@ -163,66 +176,47 @@ router.get('/2', function(req, res, next) {
       var FB = require('fb');
       FB.api(CREDENTIALS.facebook.brandId + '/feed', { access_token: CREDENTIALS.facebook.access_token }, function(res) {                    
           if (res.data[0]) {
-          	data = pushToArray(data, 2, res.data[0].id, res.data[0].story, res.data[0].story, res.data[0].created_time, "")
+          	data = pushToArray(data, 'facebook', res.data[0].id, res.data[0].story, res.data[0].story, res.data[0].created_time, "")
           }
           callback(null, data);
       });		    	
 	    },
-    function(data, callback) {
-              var pg_client = new pg.Client({
-                  user: CREDENTIALS.pg.user,
-                  password: CREDENTIALS.pg.password,
-                  database: CREDENTIALS.pg.database,
-                  port:  CREDENTIALS.pg.port,
-                  host:  CREDENTIALS.pg.host,
-                  ssl: false
-              });
-
-              pg_client.connect(function(err) {
-                  if (err) {
-                  	callback('could not connect to postgres', data)                        
-                  }
-                  var count = 0;
-                  var len = data.length;
-                  for (var i = 0; i < len; i++) {
-                      var string = '' + data[i].title;
-                      string = string.replace("'", "\'");
-                      var length = 199;
-                      var title = string.substring(0, length);
-
-                      var description = escape(data[i].description);
-                      var temp = new Date();
-                      var defaultDate = temp.toUTCString();
-                      var query = 
-                      "insert into Item ( SourceID, AreaID, ItemTypeID, Title,  Description, ViewCount, SourceURL, CoordinateXY, "+
-                      "LabelAlignment,  CreatedUTC, SourceCreatedUTC) SELECT  '" + 
-                      data[i].sourceID + "', 24, "+data[i].itemType+", '" + title + "',  '" + description + "', 0, '" + 
-                      data[i].sourceUrl + "', '(70,4)', 'LEFT','"+defaultDate+"',  '" + data[i].sourceCreatedUTC + 
-                      "' WHERE NOT EXISTS (SELECT sourceid FROM Item  WHERE sourceid = '" + data[i].sourceID + "' AND itemtypeid='" + 
-                      data[i].itemType + "');";
-                          console.log(query)
-
-                      pg_client.query(query, function(err, result) {
-                          if (err) {
-                              return console.error('error running query', err);
-                          } else {
-                              console.log("input success!");
-                          }
-                          count++;
-                          if (count == len) {
-                              pg_client.end();
-                              data = [];
-                          }
-                      });                        
-                  }
-                  callback(null, data);
-              });		    	
+    function(data, callback) {    	
+    	sourceids = data.map(function(item){ return item['sourceid'] })
+	    	/*
+		    	Item.destroy({
+		    		where: {
+		    			sourceid: {$in: sourceids}
+		    		}
+		    	}).then(function(rows){
+						Item.bulkCreate(data).then(function(items) {
+			  			//console.log(items);
+			  			//res.send({status: "200", data: item})
+			  			callback(null, items);
+						})
+		    	})			
+				*/
+	    	Item.findAll({
+	    		where: {
+	    			sourceid: {$in: sourceids}
+	    		},
+	    		attributes: ['sourceid']
+	    	}).then(function(rows){
+	    		existingdata = rows.map(function(row){ return row['dataValues']['sourceid'].toString() })
+	    		for(var i = data.length - 1; i >= 0; i--) {	
+				    if(existingdata.indexOf(data[i]['sourceid'].toString()) > -1){
+				      data.splice(i, 1)
+				    }
+					}	    		
+					Item.bulkCreate(data).then(function(items) {		  		
+		  			callback(null, data);
+					})
+	    	})
 	    }
 	], function (err, result) {
 	    if(err) res.send({status: "error", err: err})
 	    else	  res.send({status: "200", data: data})
 	});		
 })
-
 
 module.exports = router;
--- db.sql
+++ db.sql
@@ -93,3 +93,20 @@ CREATE TABLE media
       REFERENCES mediatype (mediatypeid) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+ALTER TABLE item ADD CONSTRAINT uniqsourceid UNIQUE (sourceid);
+
+
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (1, 'Brand', 'http://www.proximityinsight.com/wp-content/uploads/2016/06/Cartier-logo-c.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (2, 'Facebook', 'http://lcps.k12.nm.us/wp-content/themes/lcps/fd8ca743ed4abc6e6aaaa4622772fe96/lcps-share/images/facebook-small.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (3, 'Youtube', 'http://www.massageenvy.com/Common/Img/clinics/162/youtube_logo_small.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (4, 'Instagram', 'http://maddisonbeahome.com/wp-content/uploads/2016/01/logo-instagram.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (5, 'Twitter', 'http://www.seai.ie/images_upload/Schools/Post_Primary/One-Good-Idea-Project/icon-twitter.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (6, 'Pinterest', 'https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-pinterest-m.png');
+INSERT INTO itemtype (itemtypeid, name, iconurl) VALUES (7, 'Tumblr', 'https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-tumblr-m.png');
+
+
+INSERT INTO mediatype (mediatypeid, type) VALUES (1, 'Video');
+INSERT INTO mediatype (mediatypeid, type) VALUES (2, 'Image');
+
+/* heroku pg:psql   to login to heroku database */
